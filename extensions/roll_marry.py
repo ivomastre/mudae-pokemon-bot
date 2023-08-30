fbd1f9713a1b901cbfa8994119a57e17b2650746
Ivo Augusto <ivomastre@gmail.com>
2023-08-30 19:15:29 -0300

    feat: roll marry extension

---
+++ extensions/roll_marry.py
@@ -0,0 +1,35 @@
+from discord.ext import tasks, commands
+import os
+import asyncio
+from index import MudaeBot
+
+
+class RollMarry(commands.Cog):
+    def __init__(self, bot: MudaeBot):
+        self.bot = bot
+        self.roll.start()
+
+    @tasks.loop(seconds=10)
+    async def roll(self):
+        if self.bot.state.timer.rolls_left == 0:
+            return
+
+        channel = await self.bot.fetch_channel(os.getenv("CHANNEL_ID", None))
+
+        async def send_marry_roll(channel):
+            await channel.send("$m")
+
+        for _ in range(self.bot.state.timer.rolls_left):
+            self.bot.logger.info("SENDING_MARRY_COMMAND")
+
+            await asyncio.sleep(2)
+
+            self.bot.loop.create_task(send_marry_roll(channel))
+
+            self.bot.state.timer.rolls_left -= 1
+
+        self.bot.logger.info("ROLL_SUCCESS")
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(RollMarry(bot))
