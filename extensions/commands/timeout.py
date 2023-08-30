8981b019b198e4bb2993413db4f0d577806e0d17
Ivo Augusto <ivomastre@gmail.com>
2023-08-30 11:34:59 -0300

    feat: removed timeout user pings

--- extensions/commands/timeout.py
+++ extensions/commands/timeout.py
@@ -2,41 +2,27 @@ from discord.ext import tasks, commands
 import os
 import asyncio
 import time
+from index import MudaeBot
 
 
 class Timeout(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: MudaeBot):
         self.bot = bot
-
-    async def alert_finished_timeout(self, user_id: int):
-        # Ping the user
-        await self.bot.get_channel(int(os.getenv("SNIPE_CHANNEL_ID"))).send(
-            f"<@{user_id}> acabou o timeout"
-        )
 
     @commands.command()
     async def timeout(self, ctx: commands.Context):
         if ctx.channel.id != int(os.getenv("SNIPE_CHANNEL_ID")):
             return
-        
-        # Ignore if the user is already in timeout
-        if time.time() < self.bot.state.timeout:
-            return
 
         timeout_timer = int(os.getenv("TIMEOUT_TIMER"))
 
-        # 1 minutes timeout
         next_timeout = time.time() + timeout_timer
 
+        self.bot.logger.info(
+            "TIMEOUT_COMMAND_RECEIVED", extra={"timeout": timeout_timer}
+        )
+
         self.bot.state.timeout = next_timeout
-
-        # Send a message to the user
-        await ctx.reply("blz")
-
-        # Wait for the timeout to finish
-        # And then ping the user
-        await asyncio.sleep(timeout_timer)
-        asyncio.create_task(self.alert_finished_timeout(ctx.author.id))
 
 
 async def setup(bot: commands.Bot):
